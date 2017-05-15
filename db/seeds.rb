b03d3ddd0e9dcb975e18d72d2519cd37c30778a6
m-shehata <mido_matrix6000@yahoo.com>
2017-05-15 22:21:24 +0200

    Adding seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-AdminUser.create!(email: 'mohamed@shehata.com', password: '123456', password_confirmation: '123456')
+User.create!(name: 'Mohamed', email: 'mohamed@shehata.com', password: '123456', password_confirmation: '123456', role: "Administrator", superadmin: true)
